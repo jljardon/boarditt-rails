9f76752cab6fdf264a5f6c708b0126e101b261ad
jljardon <github email address>
2018-05-17 18:26:32 -0400

    add Post model
    Added Post model and basic tests.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_17_200119) do
+ActiveRecord::Schema.define(version: 2018_05_17_221941) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
---
+++ spec/models/post_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  let(:content) { "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dapibus, nulla vel condimentum ornare, arcu lorem hendrerit purus, ac sagittis ipsum nisl nec erat. Morbi porta sollicitudin leo, eu cursus libero posuere ac. Sed ac ultricies ante. Donec nec nulla ipsum. Nunc eleifend, ligula ut volutpat." }
+
+  let(:valid_attrs) do
+    {
+      title: "Post title for testing",
+      content: content,
+      user_id: 1,
+    }
+  end
+
+  context "attributes" do
+    let(:valid_post) { Post.new(valid_attrs) }
+
+    it "has a title" do
+      expect(valid_post.title).to eq("Post title for testing")
+    end
+
+    it "has content" do
+      expect(valid_post.content).to eq(content)
+    end
+
+    it "belongs to a user" do
+      expect(valid_post.user_id).to eq(1)
+    end
+  end
+end
