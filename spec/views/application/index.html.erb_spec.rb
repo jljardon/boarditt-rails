3ee2cdbeca7cdf820cc1d36ff60e0b15e1cbbeb5
jljardon <github email address>
2018-05-18 23:53:26 -0400

    add login form
    Added login form, link to create a new user and User routes.

--- spec/views/application/index.html.erb_spec.rb
+++ spec/views/application/index.html.erb_spec.rb
@@ -4,10 +4,18 @@ RSpec.describe "application/index", type: :view do
 
   context "Application" do
 
-    it 'shows the home page' do
+    it 'loads the home page an login form' do
       visit "/"
+      expect(page).to have_text("Username:")
+      expect(page).to have_text("Password:")
+      expect(page).to have_field 'user[username]'
+      expect(page).to have_field 'user[password]'
+      expect(page).to have_button 'Login'
+      expect(page).to have_link 'Sign up', href: new_user_path
       expect(page.status_code).to eq(200)
     end
+
+
 
   end
 end
