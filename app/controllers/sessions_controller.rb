e484ff4a4c1e2ff6e07696ad6212d80d47a0d379
jljardon <github email address>
2018-05-23 17:00:45 -0400

    add oauth facebook strategy
    Added OAuth strategy for facebook, generated route, creates a user with a secure password if its the first login, added facebook to home.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,33 @@
 class SessionsController < ApplicationController
   def create
-    @user = User.find_by(username: params[:user][:username])
-    if @user && @user.authenticate(params[:user][:password])
+    if auth['uid']
+      @user = User.find_by(email: auth['info']['email'], username: auth['info']['name'])
+      unless @user
+        @user = User.create(username: auth['info']['name'],
+                            email:  auth['info']['email'],
+                            password:  SecureRandom.urlsafe_base64)
+      end
       session[:user_id] = @user.id
-      redirect_to user_path(@user), notice: 'Welcome back to boarditt!'
+      redirect_to user_path(@user), notice: 'Welcome to boarditt!'
     else
-      redirect_to root_path
+      @user = User.find_by(username: params[:user][:username])
+      if @user && @user.authenticate(params[:user][:password])
+        session[:user_id] = @user.id
+        redirect_to user_path(@user), notice: 'Welcome back to boarditt!'
+      else
+        redirect_to root_path
+      end
     end
   end
 
   def destroy
     session[:user_id] = nil
     redirect_to root_path
+  end
+
+  private
+
+  def auth
+    request.env['omniauth.auth']
   end
 end
